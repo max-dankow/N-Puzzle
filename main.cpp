4bc5bc61dc16b7e93d5e367ee1bdfc4a7ba40cd3
Maxim Dankovtsev <max.dankow@gmail.com>
2015-04-20 20:14:18 +0300

    Enum added.

--- main.cpp
+++ main.cpp
@@ -28,16 +28,26 @@ bool operator <(const SetNode &a, const SetNode &b)
     return (a.priority < b.priority || (a.priority == b.priority && a.state < b.state));
 }
 
-const int D_UP = 0;
-const int D_LEFT = 1;
-const int D_RIGTH = 2;
-const int D_DOWN = 3;
+enum Directions
+{
+    UP = 0,
+    LEFT = 1,
+    RIGTH = 2,
+    DOWN = 3
+};
+
+struct Offset
+{
+    int x[4];
+    int y[4];
+};
+
+char* direction_names[4] = {"UP", "LEFT", "DOWN", "RIGHT"};
 
 class CPuzzleSolver
 {
 public:
     void print_field(State state);
-
     CPuzzleSolver(ssize_t new_size);
     State get_state(const std::vector<int> &data);
     std::vector<int> solve_puzzle(const State &start, const State &goal);
@@ -46,12 +56,19 @@ private:
     ssize_t size;
     ssize_t element_number;
     State start, goal;
+    Offset moves = {{0, -1, 0, 1}, {-1, 0, 1, 0}};
 
-    State move_to(State state, int direction);
+    State move_to(State state, Directions direction);
     long long estimation(State state);
     int get_direction(State start, State finish);
     std::vector<int> restore_way(const std::map<State, Info> &closed, State goal);
+    bool is_valid_coordinates(int x, int y);
 };
+
+bool CPuzzleSolver::is_valid_coordinates(int x, int y)
+{
+    return (x >= 0 && y >= 0 && x < size && y < size);
+}
 
 CPuzzleSolver::CPuzzleSolver(ssize_t new_size)
 {
@@ -89,7 +106,7 @@ State CPuzzleSolver::get_state(const std::vector<int> &data)
     return state;
 }
 
-State CPuzzleSolver::move_to(State state, int direction)
+State CPuzzleSolver::move_to(State state, Directions direction)
 {
     std::vector<int> table;
     State local_state = state;
@@ -109,46 +126,14 @@ State CPuzzleSolver::move_to(State state, int direction)
     }
 
     assert(free_index < element_number);
-    ssize_t offset = 0;
 
-    switch (direction)
+    int new_x = free_index % size + moves.x[direction];
+    int new_y = free_index / size + moves.y[direction];
+
+    if (is_valid_coordinates(new_x, new_y))
     {
-    case D_DOWN:
-        if ((free_index / size) + 1 < size)
-        {
-            offset = size;
-        }
-        break;
-
-    case D_UP:
-        if ((free_index / size) - 1 >= 0)
-        {
-            offset = -size;
-        }
-        break;
-
-    case D_LEFT:
-        if ((free_index % size) - 1 >= 0)
-        {
-            offset = -1;
-        }
-        break;
-
-    case D_RIGTH:
-        if ((free_index % size) + 1 < size)
-        {
-            offset = 1;
-        }
-        break;
+        std::swap(table[free_index], table[free_index + moves.x[direction] + moves.y[direction] * size]);
     }
-
-    if (offset == 0)
-    {
-        return state;
-    }
-
-    std::swap(table[free_index], table[free_index + offset]);
-
     return get_state(table);
 }
 
@@ -197,26 +182,11 @@ int CPuzzleSolver::get_direction(State start, State finish)
     assert(free_index_start < element_number);
     assert(free_index_finish < element_number);
 
-    if (free_index_start == free_index_finish + 1)
+    for (int i = 0; i < 4; ++i)
     {
-        return D_RIGTH;
+        if(free_index_start - free_index_finish == moves.x[i] + moves.y[i] * size)
+            return Directions(i);
     }
-
-    if (free_index_start == free_index_finish - 1)
-    {
-        return D_LEFT;
-    }
-
-    if (free_index_start == free_index_finish + size)
-    {
-        return D_DOWN;
-    }
-
-    if (free_index_start == free_index_finish - size)
-    {
-        return D_UP;
-    }
-
     assert(false);
 }
 
@@ -264,7 +234,7 @@ std::vector<int> CPuzzleSolver::solve_puzzle(const State &start, const State &go
 
         for (int i = 0; i < 4; ++i)
         {
-            State new_state = move_to(current.state, i);
+            State new_state = move_to(current.state, Directions(i));
 
             if (closed.find(new_state) != closed.end())
                 continue;
@@ -290,21 +260,7 @@ void print_answer(const std::vector<int> &answer)
 
     for (int d : answer)
     {
-        switch (d)
-        {
-        case D_DOWN:
-            std::cout << "DOWN\n";
-            break;
-        case D_UP:
-            std::cout << "UP\n";
-            break;
-        case D_LEFT:
-            std::cout << "LEFT\n";
-            break;
-        case D_RIGTH:
-            std::cout << "RIGHT\n";
-            break;
-        }
+        std::cout << direction_names[d] << '\n';
     }
 }
 
@@ -328,7 +284,7 @@ void read_input(std::vector<int> &start_table, std::vector<int> &goal_table, ssi
 
 int main()
 {
-    //freopen("input.txt", "r", stdin);
+    freopen("input.txt", "r", stdin);
     std::vector<int> start_table, goal_table;
     ssize_t size;
     read_input(start_table, goal_table, size);
